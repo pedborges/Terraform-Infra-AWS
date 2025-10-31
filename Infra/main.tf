cbb5a0040197a1fdac6b89ad387e752ab2fe7856
Pedro Pinguelli <pedropinguelli@hotmail.com>
2025-10-31 19:42:00 -0300

    adding data to reference an IAM role

--- Infra/main.tf
+++ Infra/main.tf
@@ -25,6 +25,7 @@ resource "aws_apprunner_service" "my_demo_project_service" {
              "Jwt__Issuer"   = "MYDemoProjectURL"
              "Jwt__Audience" = "your-audience"
              "ASPNETCORE_ENVIRONMENT" = "Production"
+             "ASPNETCORE_HTTP_PORTS" = "80"
              "TokenData__SecretAPIKey"= "default_secret_key_please_change_it"
              "ConnectionStrings__DefaultConnection" = "Server=YOUR_SERVER_NAME;Database=YourDatabaseName;User Id=YOUR_USER;Password=YOUR_PASSWORD;TrustServerCertificate=True;"
         }
