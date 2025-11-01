efbd9ac57335d61a1ff35c96e5cad979e189a504
Pedro Pinguelli <pedropinguelli@hotmail.com>
2025-11-01 17:03:23 -0300

    adding fargate

--- Infra/main.tf
+++ Infra/main.tf
@@ -5,7 +5,118 @@ provider "aws" {
 resource "aws_ecr_repository" "webapi" {
   name = "demo_project_repository"
 }
+# --- Networking --------------------------------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+# --- ECS Cluster -------------------------------------------------
+resource "aws_ecs_cluster" "api_cluster" {
+  name = "myapi-cluster"
+}
+
+# --- IAM Role for Task Execution (to pull from ECR) --------------
+resource "aws_iam_role" "ecs_task_exec_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_attach" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# --- Security Group ---------------------------------------------
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --- Task Definition --------------------------------------------
+resource "aws_ecs_task_definition" "myapi_task" {
+  family                   = "myapi-task"
+  requires_compatibilities  = ["FARGATE"]
+  network_mode              = "awsvpc"
+  cpu                       = "256"   # 0.25 vCPU
+  memory                    = "512"   # 0.5 GB
+  execution_role_arn        = aws_iam_role.ecs_task_exec_role.arn
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+  container_definitions = jsonencode([
+    {
+      name      = "myapi",
+      image     = "795772440200.dkr.ecr.us-east-1.amazonaws.com/demo_project_repository:latest",
+      essential = true,
+      portMappings = [{ containerPort = 8080, protocol = "tcp" }],
+      environment = [
+        { name = "ASPNETCORE_ENVIRONMENT", value = "Production" }
+      ]
+    }
+  ])
+}
+
+# --- ECS Service -------------------------------------------------
+resource "aws_ecs_service" "myapi_service" {
+  name            = "myapi-service"
+  cluster         = aws_ecs_cluster.api_cluster.id
+  task_definition = aws_ecs_task_definition.myapi_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id]
+    security_groups = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_task_exec_attach]
+}
 
 
 
