2d980589b3f525914b4fbfa3cdfb71760f41bf51
Pedro Pinguelli <pedropinguelli@hotmail.com>
2025-11-02 00:24:55 -0300

    back to apprunner

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.24.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
 provider "aws" {
   region = "us-east-1"
 }
